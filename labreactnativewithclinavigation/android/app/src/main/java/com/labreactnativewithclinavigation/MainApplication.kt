475299f9510ea32edf5f55fb9d6930d72d8e6b40
Johanbeltranramirez <johanframirez13@gmail.com>
2024-04-05 21:02:48 -0500

    añado proyecto de stack nav02

--- labreactnativewithclinavigation/android/app/src/main/java/com/labreactnativewithclinavigation/MainApplication.kt
+++ labreactnativewithclinavigation/android/app/src/main/java/com/labreactnativewithclinavigation/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.labreactnativewhitclinavigation
+package com.labreactnativewithclinavigation
 
 import android.app.Application
 import com.facebook.react.PackageList
--- labreactnativewithclinavigation/ios/labreactnativewithclinavigationTests/labreactnativewithclinavigationTests.m
+++ labreactnativewithclinavigation/ios/labreactnativewithclinavigationTests/labreactnativewithclinavigationTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface labreactnativewhitclinavigationTests : XCTestCase
+@interface labreactnativewithclinavigationTests : XCTestCase
 
 @end
 
-@implementation labreactnativewhitclinavigationTests
+@implementation labreactnativewithclinavigationTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
