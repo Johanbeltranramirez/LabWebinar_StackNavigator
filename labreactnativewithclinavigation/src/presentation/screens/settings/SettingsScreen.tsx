1aab8e0eaedf4aa60571e8156e9878810a7b0d4e
Johanbeltranramirez <johanframirez13@gmail.com>
2024-04-05 21:42:02 -0500

    añado products y algunas correcciones

---
+++ labreactnativewithclinavigation/src/presentation/screens/settings/SettingsScreen.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from "react-native";
+import HomeScreen from "../home/HomeScreen.tsx";
+
+const SettingsScreen = () => {
+  return (
+    <View>
+      <Text>Settings-Screen</Text>
+    </View>
+  )
+}
+
+export default SettingsScreen;
